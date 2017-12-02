5e7091aaeb329a557ed2391fb3a27a79af8c0e3e
Rebeca <rebeca.qs@gmail.com>
2017-12-02 00:22:44 -0200

    added part 2 to day 1

--- day1.js
+++ day1.js
@@ -1,3 +1,4 @@
+//Part 1
 function checkNumbers(numbers) {
   numbers = numbers + numbers[0];
   var numLenght = numbers.length;
@@ -5,6 +6,22 @@ function checkNumbers(numbers) {
 
   for (i = 0; i < numLenght; i++) {
     if (numbers[i] == numbers[i + 1]) {
+      result += (+numbers[i]);
+    }
+  }
+  return result;
+}
+
+
+//Part 2
+function checkNumbers2(numbers) {
+  var numLenght = numbers.length;
+  var numbersby2 = numLenght/2;
+  numbers = numbers + numbers.substring(0, numbersby2);
+  var result = 0;
+
+  for (i = 0; i < numLenght; i++){
+    if (numbers[i] == numbers[i+numbersby2]){
       result += (+numbers[i]);
     }
   }
